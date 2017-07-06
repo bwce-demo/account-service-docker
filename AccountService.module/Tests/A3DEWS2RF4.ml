b94a5e2c2c226c73d206e553df90df2549332ebb
Rishikesh Palve <rishikeshpalve@gmail.com>
2017-07-06 10:43:07 -0400

    Add project in repo

---
+++ AccountService.module/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="/AccountService.module/Tests/AccountService.module1.bwt#/"/>
+  <EmulationDataFile>ACED00057400154765744163636F756E7444657461696C732E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574001A4163636F756E74536572766963652E6D6F64756C65312E627774</EmulationDataFile>
+</emulation:BWTFile>
