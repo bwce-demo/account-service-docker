01e3a5996e781bc2a43702c72e8d36bfff883f0c
rishikeshpalve <rishikeshpalve@gmail.com>
2017-10-17 23:38:48 +0530

    Delete Tests folder

--- AccountService.module/Tests/A3DEWS2RF4.ml
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="ASCII"?>
-<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
-  <EmulationDataFile>ACED00057400154765744163636F756E7444657461696C732E627774</EmulationDataFile>
-</emulation:BWTFile>
