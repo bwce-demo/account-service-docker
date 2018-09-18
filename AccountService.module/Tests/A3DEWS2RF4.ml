5e543e251d246383d14bea266d10530a236a892a
Rishi <rishi@demo.com>
2018-09-18 14:39:29 +0530

    Disable maven nature

--- AccountService.module/Tests/A3DEWS2RF4.ml
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="ASCII"?>
-<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
-  <CurrentLoadedTest href="AccountService.module1.bwt#/"/>
-  <EmulationDataFile>ACED000574001A4163636F756E74536572766963652E6D6F64756C65312E627774</EmulationDataFile>
-</emulation:BWTFile>
